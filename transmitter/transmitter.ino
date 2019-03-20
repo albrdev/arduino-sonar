07bc7eeaa4593952cbb89153dc3b1a211c6ccab8
albrdev <albrdev@gmail.com>
2019-03-21 00:52:41 +0100

    Implemented Bluetooth serial support

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,11 +1,15 @@
 #include <limits.h>
 #include <assert.h>
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <Servo.h>
 #include "SonicSensor.hpp"
 #include "ExtendedStepper.hpp"
+#include "HC06.hpp"
 #include "math.h"
 #include "packet.h"
 #include "Timer.hpp"
+#include "LED.hpp"
 #include "Button.hpp"
 
 float getAngle(const int steps, const int maxSteps)
@@ -19,18 +23,24 @@ const double SONICSENSOR_MAXRANGE = 4.0;
 const double SONICSENSOR_RESOLUTION = 0.003;
 const double SONICSENSOR_RANGEFACTOR = 1000000.0;
 const unsigned long int SONICSENSOR_TIMEOUT = 50000UL;
-SonicSensor sonicSensor(2, 3, SONICSENSOR_RANGEFACTOR, SONICSENSOR_TIMEOUT);
+SonicSensor sonicSensor(4, 5, SONICSENSOR_RANGEFACTOR, SONICSENSOR_TIMEOUT);
 
 ExtendedStepper stepMotor(64, 8, 9, 10, 11);
 //const double ROTATION_MAX = 360.0;
 const double ROTATION_MAX = 180.0;
 
-Timer timer(50UL, true, false);
+Servo servoMotor;
 
-Button pauseButton(4);
+HC06 bluetooth(2, 3);
+Timer timer(50UL, true, false);
+LED led(6, false);
+Button pauseButton(7);
 
 void restartRotation(void)
 {
+    Serial.println("Max. rotation reached. Reversing...");
+    Serial.flush();
+
     stepMotor.Rotate(ROTATION_MAX * -stepMotor.GetStepDirection());
 }
 
@@ -53,8 +63,8 @@ void transmitData(void)
 
     packet_sonardata_t packet;
     packet_mksonardata(&packet, distance, angle);
-    Serial.write((char *)&packet, sizeof(packet_sonardata_t));
-    Serial.flush();
+    bluetooth.Write((char *)&packet, sizeof(packet_sonardata_t));
+    bluetooth.Flush();
 }
 
 bool isPaused = true;
@@ -62,16 +72,85 @@ void togglePause(void)
 {
     isPaused = !isPaused;
 
-    if(isPaused) timer.Stop();
-    else timer.Start();
+    if(isPaused)
+    {
+        Serial.println("Paused");
+        timer.Stop();
+    }
+    else
+    {
+        Serial.println("Unpaused");
+        timer.Start();
+    }
 
+    Serial.flush();
     delay(125UL);
+}
+
+void setupBluetooth(void)
+{
+    Serial.println("Initializing Bluetooth device");
+    bluetooth.Begin(HC06_BR_9600);
+
+    Serial.println("Verifying device");
+    while(!bluetooth.Ping())
+    {
+        Serial.println("Failed");
+        led.On();
+        delay(500UL);
+    }
+
+    delay(500UL);
+    Serial.print("Version: ");
+    const char *version = bluetooth.GetVersion();
+    if(version != nullptr)
+    {
+        Serial.println(version);
+    }
+    else
+    {
+        Serial.println("N/A");
+        led.On();
+    }
+
+    delay(500UL);
+    Serial.println("Setting baud rate");
+    while(!bluetooth.SetBaudRate(HC06BaudRate::HC06_BR_9600))
+    {
+        Serial.println("Failed");
+        led.On();
+        delay(500UL);
+    }
+
+    delay(500UL);
+    Serial.println("Setting PIN");
+    while(!bluetooth.SetPIN("1357"))
+    {
+        Serial.println("Failed");
+        led.On();
+        delay(500UL);
+    }
+
+    delay(500UL);
+    Serial.println("Setting name");
+    while(!bluetooth.SetName("ArduinoSonar"))
+    {
+        Serial.println("Failed");
+        led.On();
+        delay(500UL);
+    }
+
+    Serial.println("");
+    led.Off();
 }
 
 void setup(void)
 {
     // 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200
     Serial.begin(9600, SERIAL_8N1);
+    //servoMotor.attach(8);
+
+    setupBluetooth();
 
     stepMotor.setSpeed(120);
     stepMotor.SetOnEndRotationCallback(restartRotation);
@@ -79,6 +158,24 @@ void setup(void)
 
     timer.SetTriggerCallback(transmitData);
     pauseButton.SetReleaseCallback(togglePause);
+
+    Serial.println("Press pause button to start");
+    Serial.println("");
+    Serial.flush();
+}
+
+void servogo(void)
+{
+    int pos;
+    for(pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  // in steps of 1 degree
+        servoMotor.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+    }
+    for(pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+        servoMotor.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+    }
 }
 
 void loop(void)
